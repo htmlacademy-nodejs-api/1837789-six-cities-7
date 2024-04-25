ef59c3b1dd76dfbfc466b33daa553e76f0124e0d
Алина Вдовиченко <alinavdovichenko.ru@gmail.com>
2024-04-25 14:03:11 +0500

    1.2.2. Раскрасbn вывод информации в консоли.

--- src/cli/commands/help.command.ts
+++ src/cli/commands/help.command.ts
@@ -1,4 +1,5 @@
 import { Command } from './command.interface.js';
+import chalk from 'chalk';
 
 export class HelpCommand implements Command {
   public getName(): string {
@@ -7,13 +8,13 @@ export class HelpCommand implements Command {
 
   public async execute(..._parameters: string[]): Promise<void> {
     console.info(`
-        Программа для подготовки данных для REST API сервера.
-        Пример:
-            cli.js --<command> [--arguments]
+        ${chalk.rgb(255, 0, 198)('Программа для подготовки данных для REST API сервера.')}
+        ${chalk.rgb(188, 188, 188)('Пример:')}
+          ${chalk.rgb(188, 188, 188)('cli.js --<command> [--arguments]')}
         Команды:
-            --version:                   # выводит номер версии
-            --help:                      # печатает этот текст
-            --import <path>:             # импортирует данные из TSV
+            ${chalk.rgb(112, 255, 0)('--version:                   # выводит номер версии')}
+            ${chalk.rgb(2, 212, 225)('--help:                      # печатает этот текст')}
+            ${chalk.rgb(85, 37, 187)('--import <path>:             # импортирует данные из TSV')}
     `);
   }
 }
--- src/cli/commands/version.command.ts
+++ src/cli/commands/version.command.ts
@@ -1,5 +1,6 @@
 import { readFileSync } from 'node:fs';
 import { resolve } from 'node:path';
+import chalk from 'chalk';
 
 import { Command } from './command.interface.js';
 
@@ -39,7 +40,7 @@ export class VersionCommand implements Command {
   public async execute(..._parameters: string[]): Promise<void> {
     try {
       const version = this.readVersion();
-      console.info(version);
+      console.info(chalk.green(version));
     } catch (error: unknown) {
       console.error(`Failed to read version from ${this.filePath}`);
 
