b7fa738b7d2eaff5726cda6be2ffd1846b2c39e8
Алина Вдовиченко <alinavdovichenko.ru@gmail.com>
2024-05-03 18:06:56 +0500

    3.1.3. Избавится от присвоения переменной в условии цикла.

--- src/shared/libs/file-reader/tsv-file-reader.ts
+++ src/shared/libs/file-reader/tsv-file-reader.ts
@@ -67,10 +67,12 @@ export class TSVFileReader extends EventEmitter implements FileReader {
 
     for await (const chunk of readStream) {
       remainingData += chunk.toString();
+      nextLinePosition = remainingData.indexOf('\n');
 
-      while ((nextLinePosition = remainingData.indexOf('\n')) >= 0) {
+      while (nextLinePosition >= 0) {
         const completeRow = remainingData.slice(0, nextLinePosition + 1);
         remainingData = remainingData.slice(++nextLinePosition);
+        nextLinePosition = remainingData.indexOf('\n');
         importedRowCount++;
 
         const parsedOffer = this.parseLineToOffer(completeRow);
