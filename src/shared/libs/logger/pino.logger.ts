3d5dbed256e85ad4f8bd1d94210d956992f36dec
Алина Вдовиченко <alinavdovichenko.ru@gmail.com>
2024-05-06 14:54:34 +0500

    3.1.13. Настроит контейнер.

--- src/shared/libs/logger/pino.logger.ts
+++ src/shared/libs/logger/pino.logger.ts
@@ -1,9 +1,11 @@
 import { Logger as PinoInstance, pino, transport } from 'pino';
 import { resolve } from 'node:path';
+import { injectable } from 'inversify';
 
 import { Logger } from './logger.iterface.js';
 import { getCurrentModuleDirectoryPath } from '../../helpers/index.js';
 
+@injectable()
 export class PinoLogger implements Logger {
   private readonly logger: PinoInstance;
 
@@ -28,6 +30,7 @@ export class PinoLogger implements Logger {
     });
 
     this.logger = pino({}, multiTransport);
+    this.logger.info('Logger created…');
   }
 
   public debug(message: string, ...args: unknown[]): void {
